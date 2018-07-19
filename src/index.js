1d852fdc1404c9348c0683b582775f23342b73cc
Luke Scott <luke.m.scott@gmail.com>
2018-07-20 09:50:28 +1000

    adds library

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const striptags = require("striptags");
+
+const TAGS_TO_BREAK_ON = [
+    "p",
+    "div",
+    "br",
+    "hr",
+    "title",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "ol",
+    "ul",
+    "li"
+];
+
+module.exports = (text = "") =>
+    [text]
+        .map(val => striptags(val, TAGS_TO_BREAK_ON))
+        .map(val => striptags(val, [], "\n"))
+        .map(val => striptags(val, ["&nbsp;"], " "))
+        .map(val => val.replace(/\&nbsp\;/g, ""))
+        .map(val => val.replace(/\n\n/g, "\n"))
+        .map(val => val.replace(/\n$/, ""))[0];
