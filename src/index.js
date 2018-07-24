ac5b530d69ec4417ae5fc938513d9e69a91b934b
Luke Scott <luke.m.scott@gmail.com>
2018-07-25 04:19:03 +1000

    replaces html spaces with spaces

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ module.exports = (text = "") =>
         .map(val => striptags(val, TAGS_TO_BREAK_ON))
         .map(val => striptags(val, [], "\n"))
         .map(val => striptags(val, ["&nbsp;"], " "))
-        .map(val => val.replace(/\&nbsp\;/g, ""))
+        .map(val => val.replace(/\&nbsp\;/g, " "))
         .map(val => val.replace(/\n\n/g, "\n"))
         .map(val => val.replace(/\n$/, ""))
         .map(val => (val.startsWith("\n") ? val.slice(1) : val))[0];
